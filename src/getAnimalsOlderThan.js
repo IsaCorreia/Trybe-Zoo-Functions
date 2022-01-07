3693f2a851f9549d012f85bc60c6889b9576ce4b
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-07 17:58:09 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // Procura em SPECIES, a especie pelo nome dado, entra na chave RESIDENTS
+  const animalsBySpeciesName = species.find((especie) => especie.name === animal).residents;
+  // Procura na chave RESIDENTS se TODOS os membros tem idade maior ou igual ao número dado
+  return animalsBySpeciesName.every((resident) => resident.age >= age);
 }
 
+console.log(`Todos os animais são mais velhos que X?: ${getAnimalsOlderThan('otters', 10)}`);;
 module.exports = getAnimalsOlderThan;
