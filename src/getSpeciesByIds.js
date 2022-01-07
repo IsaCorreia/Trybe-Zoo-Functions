8900c3083e5452b6531b98841987cf95e1ac69db
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-06 21:24:06 -0300

    Req 1 - parcial

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+
+  const filterSpecies = data.species.filter((ids) => id == ids));
+
+  const {id, name} = data.species;
+  console.log(id, name);
+  data.species.map( (id) => {} );
 }
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 
 module.exports = getSpeciesByIds;
 // Requisito 1!
