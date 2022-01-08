dae29b470fcc8bc1976dbdd0ed540771042043d6
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-08 00:23:03 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  if (employees.find((pers) => pers.managers.includes(id))) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    return employees
+      .filter((pers) => `${pers.managers}` === `${managerId}`) // Utilizando template literals para igualar os tipos de variável (ESLint implicando)
+      .map((pers) => `${pers.firstName} ${pers.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
+// console.log(getRelatedEmployees(stephanieId));
 module.exports = { isManager, getRelatedEmployees };
