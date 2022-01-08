c205b9b54d1dbeca6a7bd2a86128845af21c129b
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-08 00:36:42 -0300

    Req 5 - parcial

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const arg1 = { specie: 'penguins' };
+const arg2 = { specie: 'giraffes', sex: 'female' };
+
+function countAnimals(...animal) {
+  console.log(species.contains(animal).residents)
 }
 
+console.log(`Teste 1: ${countAnimals(arg1)}`);
+// console.log(`Teste 2, 'lions' 'otters': ${countAnimals('lions', 'otters')}`);
 module.exports = countAnimals;
