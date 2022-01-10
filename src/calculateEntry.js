dfbb8b3e6d7df0b4f210b9540452a83e50d72d44
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-09 22:01:42 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,44 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+// Separa as idades dos visitantes
+function countAges(entrants) {
+  const ages = [0, 0, 0];
+  entrants.forEach((el) => {
+    if (el.age < 18) {
+      ages[0] += 1;
+    } else if (el.age >= 18 && el.age < 50) {
+      ages[1] += 1;
+    } else {
+      ages[2] += 1;
+    }
+  });
+  return ages;
 }
 
+// Chama a função acima, e monta o objeto com as idades separadas
+function countEntrants(entrants) {
+  if (entrants !== undefined && entrants.length > 0) {
+    const ages = countAges(entrants);
+    return { child: ages[0],
+      adult: ages[1],
+      senior: ages[2],
+    };
+  }
+  return 0;
+}
+
+// Desconstrói o objeto gerado por countEntrants para manusear melhor
+// Multiplica os valores das entradas pelos valores pŕe-estabelecidos
+// Retorna a soma de tudo
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants !== undefined && entrants.length > 0) {
+    const { child, adult, senior } = countEntrants(entrants);
+    const childPrices = child * prices.child;
+    const adultPrices = adult * prices.adult;
+    const seniorPrices = senior * prices.senior;
+    return childPrices + adultPrices + seniorPrices;
+  }
+  return 0;
 }
 
 module.exports = { calculateEntry, countEntrants };
