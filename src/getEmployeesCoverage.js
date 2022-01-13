15d6f12213a30757a91e89185690b6e11f73ce87
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-12 22:00:14 -0300

    Req 10 - parcial (anotacoes)

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,4 +1,21 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
+/*
+  Input: ()
+  Output: 1 Array de objetos contendo:
+   [ 
+     {
+     ¹ id,
+     ¹ nome completo,
+     ² especies que funcionario cuida,
+     ³ locais onde as especies se encontram.
+    },
+   ]
+  
+  Dicas?
+  ¹ "getEmployee"
+  ² "getOldestFromSpecies"
+  ³ "getAnimalMap"
+*/
 
 function getEmployeesCoverage() {
   // seu código aqui
