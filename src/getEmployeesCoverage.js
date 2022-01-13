96c5ebf94f20631fa70a8bb6fa3460828f04822f
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-12 22:14:02 -0300

    Req 10 - parcial

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -17,8 +17,26 @@ const { species, employees } = require('../data/zoo_data');
   ³ "getAnimalMap"
 */
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeeInfo (employee) {
+  // Para entrada: id, nome, sobrenome
+  return employees.find((elem) => elem.id == employee);
 }
+
+function getEmployeesCoverage(info) {
+  // Deve checar qual o tipo de entrada (se: nome, id, sobrenome, vazio ou inválido)
+  return getEmployeeInfo(info)
+}
+const testEmployee = employees[0];
+console.log(`
+  ==============
+  Input teste:
+  ${testEmployee.id}
+  ${testEmployee.firstName}
+  ${testEmployee.lastName}
+  ==============
+
+  Output:
+  `);
+console.log(getEmployeesCoverage(testEmployee.id));
 
 module.exports = getEmployeesCoverage;
